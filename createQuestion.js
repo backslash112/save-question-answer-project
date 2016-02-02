d2f9ced7c84466ab8a5023f916a85de2a99bce02
backslash112 <yangcun@live.com>
2016-02-02 13:22:09 +0800

    add jquery js to js file.

--- createQuestion.js
+++ createQuestion.js
@@ -7,6 +7,7 @@ document.getElementsByTagName('head')[0].appendChild(script);
 run();
 
 function run() {
+	/*
 	// create data
 	var data = JSON.stringify({
 		"question_text": "This is a question from js code.",
@@ -21,4 +22,31 @@ function run() {
 		dataType: 'json',
 		processData: false
 	});
+	*/
+	var url = "http://localhost:8000/api/question/";
+	post(url, {question_text: "This is a question from js code."});
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+         }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
 }
