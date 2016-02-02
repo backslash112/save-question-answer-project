455a19588c459fa624bf6c69f6884c752da43aa9
backslash112 <yangcun@live.com>
2016-02-02 13:13:38 +0800

    add jquery js to js file.

--- createQuestion.js
+++ createQuestion.js
@@ -1,16 +1,24 @@
-<script src="jquery-1.6.1.js"></script>
-<script src="my_jquery.js"></script>
+// add jquery
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
 
-var data = JSON.stringify({
-	"question_text": "This is a question from js code.",
-	"answer": 1
-});
+run();
 
-$.ajax({
-  url: 'http://localhost:8000/api/question/',
-  type: 'POST',
-  contentType: 'application/json',
-  data: data,
-  dataType: 'json',
-  processData: false
-});
+function run() {
+	// create data
+	var data = JSON.stringify({
+		"question_text": "This is a question from js code.",
+		"answer": 1
+	});
+	// post
+	$.ajax({
+		url: 'http://localhost:8000/api/question/',
+		type: 'POST',
+		contentType: 'application/json',
+		data: data,
+		dataType: 'json',
+		processData: false
+	});
+}
