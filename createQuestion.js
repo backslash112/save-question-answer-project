3326f8f0fb1a60fa875bebfe6d3a29a4af837e56
backslash112 <yangcun@live.com>
2016-02-02 13:08:58 +0800

    add jquery js to js file.

--- createQuestion.js
+++ createQuestion.js
@@ -1,5 +1,6 @@
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  
+<script src="jquery-1.6.1.js"></script>
+<script src="my_jquery.js"></script>
+
 var data = JSON.stringify({
 	"question_text": "This is a question from js code.",
 	"answer": 1
